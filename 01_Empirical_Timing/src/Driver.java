21fef299412792901161ad027d7e901ffa69c925
Ryan Young <ryanjeffyoung@gmail.com>
2020-09-24 19:16:51 -0700

    working sorted array list.

--- 01_Empirical_Timing/src/Driver.java
+++ 01_Empirical_Timing/src/Driver.java
@@ -1,6 +1,148 @@
-public class Driver {
-    public static void main(String[] args) {
-        SortedArrayList<Integer> intSAL = new SortedArrayList<Integer>(20);
+/***
+ * Driver to run tests and time performance
+ * of SortedArrayList
+ *
+ * @author Ryan Young
+ */
 
+public class Driver {
+    public static final String FORMAT = "%-10s%s%20s%s%20s%s%n";
+
+    public static void main(String[] args) {
+       testAdd(5000, 6);
+       testAddReverse(5000, 6);
+       testRemove(5000, 6);
+       testClear(5000, 6);
+
+    }
+
+    /***
+     * Adds elements to array in order & times performance
+     * defined number of runs. Doubles size of array after each run.
+     * Linear runtime.
+     * @param initialSize initial number of elements in array
+     * @param numRounds number of times to double test size
+     */
+    public static void testAdd(int initialSize, int numRounds){
+        int n = initialSize;
+        long prevTime = 0;
+        System.out.println("Adding in order [O(n)]");
+        System.out.println("----------------");
+        for (int i = 0; i < numRounds; i++){
+            SortedArrayList<Integer> list = new SortedArrayList<>(n);
+            long startTime = System.nanoTime();
+            for (int j = 0; j < n; j++){
+                list.add(i);
+            }
+            long endTime = System.nanoTime();
+            long totalTime = endTime - startTime;
+
+            double factor = 0;
+            if (i != 0)
+                factor = totalTime / prevTime;
+            prevTime = totalTime;
+            System.out.printf(FORMAT, "Test Size: ",n , "Time (ms): ", totalTime, "Factor: ", factor);
+            n *= 2;
+        }
+        System.out.println();
+    }
+
+    /***
+     * Adds elements to array in reverse order & times
+     * performance defined number of runs. Doubles size of array after each run.
+     * Quadratic runtime.
+     * @param initialSize initial number of elements in array
+     * @param numRounds number of times to double test size
+     */
+    public static void testAddReverse(int initialSize, int numRounds){
+        int n = initialSize;
+        long prevTime = 0;
+        System.out.println("Adding in reverse [O(n^2)]");
+        System.out.println("----------------");
+        for (int i = 0; i < numRounds; i++){
+            SortedArrayList<Integer> list = new SortedArrayList<>(n);
+            long startTime = System.currentTimeMillis();
+            for (int j = n - 1 ; j >= 0; j--){
+                list.add(j);
+            }
+            long endTime = System.currentTimeMillis();
+            long totalTime = endTime - startTime;
+
+            double factor = 0;
+            if (i != 0)
+                factor = totalTime / prevTime;
+            prevTime = totalTime;
+            System.out.printf(FORMAT, "Test Size: ",n , "Time (nano): ", totalTime, "Factor: ", factor);
+            n *= 2;
+        }
+        System.out.println();
+    }
+
+    /***
+     * Adds elements to array in order.
+     * Then removes elements individually & times performance
+     * defined number of times. Doubles size of array after each run.
+     * Quadratic runtime.
+     * @param initialSize initial number of elements in array
+     * @param numRounds number of times to double test size
+     */
+    public static void testRemove(int initialSize, int numRounds){
+        int n = initialSize;
+        long prevTime = 0;
+        System.out.println("Removing elements [O(n^2)]");
+        System.out.println("----------------");
+        for (int i = 0; i < numRounds; i++){
+            SortedArrayList<Integer> list = new SortedArrayList<>(n);
+
+            for (int j = 0; j < n; j++){
+                list.add(j);
+            }
+            long startTime = System.nanoTime();
+
+            for (int j = 0; j < n; j++){
+                list.remove(0);
+            }
+            long endTime = System.nanoTime();
+            long totalTime = endTime - startTime;
+            double factor = 0;
+            if (i != 0)
+                factor = totalTime / prevTime;
+            prevTime = totalTime;
+            System.out.printf(FORMAT, "Test Size: ",n , "Time (ms): ", totalTime, "Factor: ", factor);
+            n *= 2;
+        }
+        System.out.println();
+    }
+
+    /***
+     * Adds elements to array in order.
+     * Clears the array & times performance defined number of times.
+     * Doubles size of array after each run.
+     * Constant runtime.
+     * @param initialSize initial number of elements in array
+     * @param numRounds number of times to double test size
+     */
+    public static void testClear(int initialSize, int numRounds){
+        int n = initialSize;
+        long prevTime = 0;
+        System.out.println("Clearing list [O(1)]");
+        System.out.println("----------------");
+        for (int i = 0; i < numRounds; i++){
+            SortedArrayList<Integer> list = new SortedArrayList<>(n);
+            for (int j = 0; j < n; j++){
+                list.add(j);
+            }
+            long startTime = System.nanoTime();
+            list.clear();
+            long endTime = System.nanoTime();
+            long totalTime = endTime - startTime;
+            double factor = 0;
+            if (i != 0)
+                factor = totalTime / prevTime;
+            prevTime = totalTime;
+            System.out.printf(FORMAT, "Test Size: ",n , "Time (ms): ", totalTime, "Factor: ", factor);
+            n *= 2;
+        }
+        System.out.println();
     }
 }
--- 01_Empirical_Timing/src/SortedArrayList.java
+++ 01_Empirical_Timing/src/SortedArrayList.java
@@ -1,46 +1,97 @@
+/***
+ * Dynamic, array-backed data structure which adds
+ * elements in natural order and maintains proper
+ * capacity for number of elements to store.
+ *
+ * @author Ryan Young
+ */
+
 import java.util.AbstractList;
+public class SortedArrayList<E extends Comparable<E>> extends AbstractList<E> {
 
-public class SortedArrayList<E> extends AbstractList<E> implements Comparable{
-
-    SortedArrayList(){
-
-    }
+    private E[] data;
+    private int size = 0;
+    public static final int CAP = 16;
 
     /***
-     * Constructor.
-     * @param size initial capacity of array
+     * Initializes generic array with capacity of 16.
      */
-    SortedArrayList(int size) {
-        E[] original = (E[])new Object[size];
+    public SortedArrayList(){
+        this(CAP);
     }
 
     /***
-     * Inserts an item in the array.
-     * Dynamically sizes and sorts to place
-     * in the correct position.
+     * Initializes generic array with defined capacity.
+     * @param cap initial capacity of array
+     */
+    public SortedArrayList(int cap) {
+       data = (E[]) new Comparable[cap];
+
+    }
+
+    /***
+     * Inserts an item in the array while keeping natural
+     * ordering. Dynamically doubles capacity if adding
+     * element would go over capacity.
      * @param item to be inserted
-     * @return true if successful, else false
+     * @return true if successful
      */
-    @Override
     public boolean add(E item){
+        if (size() == capacity())
+            resize(2 * capacity());
+        int i;
+        for (i = size() - 1; (i >= 0 &&get(i).compareTo(item) > 0); i--)
+            data[i + 1] = get(i);
 
-        return false;
+        data[i + 1] = item;
+        size++;
+        return true;
     }
 
     /***
-     The capacity of the array
-     @return max number of items in array
+     * Removes element from the array list.
+     * Shrinks capacity if size less than 1/4 capacity.
+     * Never shrinks lower than initial capacity.
+     * @param i index of element to remove
+     * @return element being removed
+     * @throws IndexOutOfBoundsException
      */
-    int capacity(){
+    public E remove(int i) throws IndexOutOfBoundsException {
+        checkIndex(i, size);
+        double factor = 0.5 * capacity();
 
-        return 0;
+        if (size - 1 < (capacity() * 0.25)) {
+            if ( factor < CAP)
+                resize(CAP);
+            else resize((int)0.5 * capacity());
+        }
+
+        E temp = data[i];
+        for (int k=i; k < size - 1; k++)
+            data[k] = data[k + 1];
+        data[size-1] = null;
+        size--;
+        return temp;
     }
 
     /***
-     * Resets the size of array to 0
+     * The capacity of the array
+     * @@return max number of items in array
+     */
+    public int capacity(){
+        return data.length;
+    }
+
+    /***
+     * Resets the size of array to 0 and
+     * sets all values to null.
      */
     public void clear(){
-
+        int n = size;
+        for (int i = 0; i < n ; i++) {
+            data[i] = null;
+            size--;
+        }
     }
 
     /***
@@ -49,8 +100,9 @@ public class SortedArrayList<E> extends AbstractList<E> implements Comparable{
      * @return value of item at index
      */
     @Override
-    public E get(int index) {
-        return null;
+    public E get(int index) throws IndexOutOfBoundsException {
+        checkIndex(index, size);
+        return data[index];
     }
 
     /***
@@ -58,19 +110,9 @@ public class SortedArrayList<E> extends AbstractList<E> implements Comparable{
      * @return True if size is 0, else false
      */
     public boolean isEmpty(){
-
-        return false;
+        return size == 0;
     }
 
-//    /***
-//     * Deletes item from array.
-//     * May shift other items left.
-//     * @param index of item to remove
-//     * @return value of item being removed
-//     */
-//    public E remove(int index){
-//
-//    }
 
     /***
      * Size of the data structure
@@ -78,16 +120,37 @@ public class SortedArrayList<E> extends AbstractList<E> implements Comparable{
      */
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+
+
+    /// UTILITY METHODS
+    /***
+     * Resizes the capacity of array list by
+     * initializing a new array of defined size
+     * and copying elements over.
+     * @param cap intended capacity for new list
+     */
+    protected void resize(int cap) {
+        E[] temp = (E[]) new Comparable[cap];
+        for (int k = 0; k < size; k++)
+            temp[k] = data[k];
+        data = temp;
     }
 
     /***
-     * Compares two objects
-     * @param o the object being compared to
-     * @return -1, if object A
+     * Checks if valid index
+     * @param i index of element
+     * @param n number of elements
+     * @throws IndexOutOfBoundsException
      */
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+    protected void checkIndex(int i, int n) throws IndexOutOfBoundsException {
+        if (i < 0 || i >= n)
+            throw new IndexOutOfBoundsException("Illegal index: " + i);
     }
+
+
+
+
 }
