8329b09aab154c2d0d730e6fba81f5eace69f2d3
Ryan Young <ryanjeffyoung@gmail.com>
2020-10-20 16:37:12 -0700

    Setup structure with proper javadoc comments

--- 02_Linked_List/src/List.java
+++ 02_Linked_List/src/List.java
@@ -4,5 +4,18 @@
  * @author Ryan Young
  */
 
-public interface List {
+public interface List<E> {
+    public boolean add(E datum);
+    public boolean add(List<E> other);
+    public boolean addFirst(E datum);
+    public boolean addLast(E datum);
+    public void clear();
+    public int count(E target);
+    public E get(int index);
+    public boolean isEmpty();
+    public E remove(int index);
+    public void reverse();
+    public E set(int index, E value);
+    public int size();
+    public void sort();
 }
--- 02_Linked_List/src/SinglyLinkedList.java
+++ 02_Linked_List/src/SinglyLinkedList.java
@@ -5,5 +5,158 @@
  * @author Ryan Young
  */
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> implements List {
+    /***
+     * Nested node class
+     * @param <E>
+     */
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+        public Node(E e, Node<E> n){
+            element = e;
+            next = n;
+        }
+
+        public E getElement(){
+            return element;
+        }
+        public Node<E> getNext(){
+            return next;
+        }
+        public void setNext(Node<E> n){
+            next = n;
+        }
+    }
+
+//    instance variables
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    /***
+     * Appends datum to end of list
+     * @param datum to be added to list
+     * @return true if successfully added
+     */
+    @Override
+    public boolean add(Object datum) {
+        return false;
+    }
+
+    /***
+     * Appends every datum from list,
+     * in order, to current list
+     * @param other list to add
+     * @return true if successful
+     */
+    @Override
+    public boolean add(List other) {
+        return false;
+    }
+
+    /***
+     * Adds to front of list
+     * @param datum to be added
+     * @return true if successful
+     */
+    @Override
+    public boolean addFirst(Object datum) {
+
+        return false;
+    }
+
+    /***
+     * Adds to end of list
+     * @param datum to be added
+     * @return true if successful
+     */
+    @Override
+    public boolean addLast(Object datum) {
+        return false;
+    }
+
+    /***
+     * Resets list to empty state
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    /***
+     * Counts number of instances of target
+     * in list
+     * @param target value to check for occurrences
+     * @return number of occurrences
+     */
+    @Override
+    public int count(Object target) {
+        return 0;
+    }
+
+    /***
+     * Get element at specified index in list
+     * @param index to get element from
+     * @return the element
+     */
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    /***
+     * Checks if no elements in list
+     * @return true if empty
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /***
+     * Removes item from list
+     * @param index to remove item
+     * @return element removed
+     */
+    @Override
+    public Object remove(int index) {
+        return null;
+    }
+
+    /***
+     * Reverses the list direction
+     */
+    @Override
+    public void reverse() {
+
+    }
+
+    /***
+     * Changes value of specified index
+     * @param index of element to change
+     * @param value to change element to
+     * @return original value of element
+     */
+    @Override
+    public Object set(int index, Object value) {
+        return null;
+    }
+
+    /***
+     * Number of items in list
+     * @return number of items in list
+     */
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    /***
+     * Places list in natural order
+     */
+    @Override
+    public void sort() {
+
+    }
 }
